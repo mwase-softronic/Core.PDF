0a0e55432eeee4acf626de6f363282bd8a1ec58f
Wase Markus <Markus.Wase@softronic.se>
2016-09-21 10:34:51 +0200

    Initial commit

---
+++ Core.PDF/Helpers/PdfGenerator.cs
@@ -0,0 +1,28 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.PDF.Helpers
+{
+    public class ITextSharpPdfGenerator
+    {
+        public static byte[] GeneratePdf(string html)
+        {
+            var stream = new MemoryStream();
+            var document = new Document(PageSize.A4);
+            document.SetMargins(0, 0, 0, 0);
+            var writer = PdfWriter.GetInstance(document, stream);
+            document.Open();
+            var strHtml = new StringReader(html);
+            iTextSharp.tool.xml.XMLWorkerHelper.GetInstance().ParseXHtml(writer, document, strHtml);
+            document.Close();
+
+            return stream.ToArray();
+        }
+    }
+}
